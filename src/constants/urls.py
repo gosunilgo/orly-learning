a5400ac6b1029a89122e7bf06e8a386f80c0efb9
Alchemy-Meister <jesusesma@gmail.com>
2020-05-05 22:44:21 +0200

    remove enum inheritance

--- src/constants/urls.py
+++ src/constants/urls.py
@@ -1,17 +1,12 @@
-from enum import Enum
-
 from urllib.parse import urljoin
 
-class Url(Enum):
-    _ignore_ = "SCHEME, OREILLY_BASE_HOST"
+class Url():
 
-    SCHEME = 'https://'
-    OREILLY_BASE_HOST = 'oreilly.com'
+    __SCHEME = 'https://'
+    __OREILLY_BASE_HOST = 'oreilly.com'
 
-    API = '{}api.{}'.format(SCHEME, OREILLY_BASE_HOST)
-    OREILLY = '{}www.{}'.format(SCHEME, OREILLY_BASE_HOST)
-    LEARNING = '{}learning.{}'.format(SCHEME, OREILLY_BASE_HOST)
-    LEARNING_PROFILE = urljoin(LEARNING, 'profile')
-
-    def __str__(self):
-        return self.value
+    API = '{}api.{}'.format(__SCHEME, __OREILLY_BASE_HOST)
+    OREILLY = '{}www.{}'.format(__SCHEME, __OREILLY_BASE_HOST)
+    LEARNING = '{}learning.{}'.format(__SCHEME, __OREILLY_BASE_HOST)
+    LEARNING_PROFILE = urljoin(LEARNING, 'profile/')
+    LEARNING_BOOK = urljoin(LEARNING, '/api/v1/book/')
