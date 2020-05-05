cf9c10dbfaa91118bd061f34aba2da4dac041129
Alchemy-Meister <jesusesma@gmail.com>
2020-05-05 22:45:28 +0200

    implemented headers constants

---
+++ src/constants/headers.py
@@ -0,0 +1,14 @@
+class Headers():
+    HEADERS = {
+        'Accept': (
+            'text/html,application/xhtml+xml,application/xml;'
+            'q=0.9,image/webp,image/apng,*/*;q=0.8'
+        ),
+        'Accept-Encoding': 'gzip, deflate',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': (
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/80.0.3987.163 Safari/537.36'
+        )
+    }
