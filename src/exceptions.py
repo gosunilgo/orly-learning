b8bff4a649e2a3fb58dee5f4fb4de744827a09c5
Alchemy-Meister <jesusesma@gmail.com>
2020-05-07 00:18:30 +0200

    implemented subscription expired exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -7,3 +7,7 @@ class PasswordError(Exception):
 class InvalidSession(Exception):
     def __init__(self):
         super().__init__('the provided session is not valid')
+
+class SubscriptionExpiredError(Exception):
+    def __init__(self):
+        super().__init__('trial subscription has expired')
