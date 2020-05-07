99dd30cab4176e0bd8ab9c14b2d65f73a8760f5e
Alchemy-Meister <jesusesma@gmail.com>
2020-05-07 21:42:35 +0200

    removed unnecessary imports and renamed auth and book as handlers

--- src/oreilly_learning_client.py
+++ src/oreilly_learning_client.py
@@ -1,47 +1,40 @@
-import json
-from http.cookies import SimpleCookie
-from urllib.parse import parse_qs, urljoin, urlparse, quote_plus
-
-from requests import Session
-from requests.exceptions import RequestException
-
-from .auth import Auth
-from .book import Book
-from .constants.urls import Url
+from .handler import AuthHandler, BookHandler
 
 class OReillyLearningClient():
 
     def __init__(self, session=None, proxy=None):
         self.proxy = proxy
 
-        self.auth = Auth(session, proxy)
-        self.book = Book(session)
+        self.auth_handler = AuthHandler(session, proxy)
+        self.book_handler = BookHandler(session)
 
     def login(self, email, password):
-        self.set_session(self.auth.login(email, password))
+        self.set_session(self.auth_handler.login(email, password))
 
     def logout(self):
-        self.set_session(self.auth.logout(), set_proxy=False)
+        self.set_session(self.auth_handler.logout(), set_proxy=False)
 
     def register(self, fields_dict):
-        self.set_session(self.auth.register(fields_dict), set_proxy=False)
+        self.set_session(
+            self.auth_handler.register(fields_dict), set_proxy=False
+        )
 
     def get_book_info(self, book_id):
-        return self.book.get_info(book_id)
+        return self.book_handler.get_info(book_id)
 
     def get_book_chapters_info(self, book_id):
-        return self.book.get_chapters_info(book_id)
+        return self.book_handler.get_chapters_info(book_id)
 
     def set_session(self, session, set_proxy=True):
         if session and set_proxy:
             session.proxies = self.proxy
 
-        self.auth.session = session
-        self.book.session = session
+        self.auth_handler.session = session
+        self.book_handler.session = session
 
     def set_proxy(self, proxy):
         self.proxy = proxy
 
-        self.auth.proxy = self.proxy
-        self.auth.session.proxies = self.proxy
-        self.book.session.proxies = self.proxy
+        self.auth_handler.proxy = self.proxy
+        self.auth_handler.session.proxies = self.proxy
+        self.book_handler.session.proxies = self.proxy
