f17803671764c1320707ff0ad8e5cec9eaac4530
Alchemy-Meister <jesusesma@gmail.com>
2020-05-10 19:06:30 +0200

    inherit from abstract handler

--- src/handler/auth_handler.py
+++ src/handler/auth_handler.py
@@ -3,16 +3,16 @@ from http.cookies import SimpleCookie
 from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
-from requests import Session
+from requests import Response, Session
 from requests.exceptions import RequestException
 
 from ..constants.headers import Headers
 from ..constants.urls import Url
-from ..exceptions import (
-    EmailError, PasswordError, InvalidSession, SubscriptionExpiredError
-)
+from ..exceptions import EmailError, PasswordError, InvalidCredentials
 
-class AuthHandler():
+from .abstract_handler import AbstractHandler
+
+class AuthHandler(AbstractHandler):
 
     OREILLY_LOGIN = urljoin(Url.OREILLY, 'member/auth/login/')
     LEARNING_LOGOUT = urljoin(Url.LEARNING, 'accounts/logout/')
@@ -20,35 +20,29 @@ class AuthHandler():
     LEARNING_REGISTER = urljoin(Url.LEARNING, 'register/')
     LEARNING_EMAIL_CHECK = urljoin(Url.LEARNING, 'check-email-availability/')
     LEARNING_PASSWORD_CHECK = urljoin(Url.LEARNING, 'check-password/')
-    LEARNING_PROFILE = urljoin(Url.LEARNING, 'profile/')
 
-    def __init__(self, session, proxy):
-        self.session = session
+    def __init__(self, session: Session, proxy: dict):
+        super().__init__(session)
         self.proxy = proxy
 
-        if self.session:
-            self.check_subscription()
-
-    def login(self, email, password):
+    def login(self, email: str, password: str) -> Session:
         self.__initialize_session()
         self.session.headers.update({'Referer': Url.LEARNING})
 
-        try:
-            login_post_response = self.session.post(
-                AuthHandler.OREILLY_LOGIN,
-                json={
-                    'email': email,
-                    'password': password
-                }
-            )
+        login_post_response = self.session.post(
+            AuthHandler.OREILLY_LOGIN,
+            json={
+                'email': email,
+                'password': password
+            }
+        )
 
-            self.__handle_broken_cookies(login_post_response)
+        if login_post_response.status_code == HTTPStatus.BAD_REQUEST.value:
+            raise InvalidCredentials()
 
-            self.check_subscription()
+        self.__handle_broken_cookies(login_post_response)
 
-            return self.session
-        except RequestException:
-            return None
+        return self.session
 
     def logout(self):
         self.session.get(AuthHandler.LEARNING_LOGOUT)
@@ -57,83 +51,67 @@ class AuthHandler():
 
         return self.session
 
-    def register(self, fields):
+    def register(self, fields: dict):
         self.__initialize_session()
         self.session.headers.update({
             'X-Requested-With': 'XMLHttpRequest',
             'Referer': AuthHandler.LEARNING_REGISTER
         })
 
-        try:
-            register_get_response = BeautifulSoup(
-                self.session.get(AuthHandler.LEARNING_REGISTER).text,
-                'html.parser'
-            )
-
-            email_check_response = self.session.get(
-                AuthHandler.LEARNING_EMAIL_CHECK,
-                params={'email': fields['email']}
-            ).json()
-
-            if not email_check_response['success']:
-                raise EmailError(email_check_response['message'])
-
-            password_name = register_get_response.find(
-                'input', {'type': 'password'}
-            )['name']
-
-            csrf_token = register_get_response.find(
-                'input', {'name': 'csrfmiddlewaretoken'}
-            )['value']
-
-            password_check_response = self.session.post(
-                AuthHandler.LEARNING_PASSWORD_CHECK,
-                data={
-                    'csrfmiddlewaretoken': csrf_token,
-                    password_name: fields['password'],
-                    'field_name': password_name
-                }
-            ).json()
-
-            if not password_check_response['valid']:
-                raise PasswordError(password_check_response['msg'])
-
-            register_post_response = self.session.post(
-                AuthHandler.LEARNING_REGISTER,
-                data={
-                    'next': '',
-                    'trial_length': register_get_response.find(
-                        id='id_trial_length'
-                    )['value'],
-                    'csrfmiddlewaretoken': csrf_token,
-                    'first_name': fields['first_name'],
-                    'last_name': fields['last_name'],
-                    'email': fields['email'],
-                    'password1': fields['password'],
-                    'country': fields['country'],
-                    'referrer': fields['referrer'],
-                    'recently_viewed_bits': '[]'
-                }
-            )
-
-            self.__handle_broken_cookies(register_post_response)
-
-            self.check_subscription()
-
-            return self.session
-        except RequestException:
-            return None
-
-    def check_subscription(self):
-        response = self.session.get(
-            AuthHandler.LEARNING_PROFILE, allow_redirects=False
+        register_get_response = BeautifulSoup(
+            self.session.get(AuthHandler.LEARNING_REGISTER).text, 'lxml'
         )
-        if response.status_code != HTTPStatus.OK.value:
-            raise InvalidSession()
-        if 'user_type": "Expired' in response.text:
-            raise SubscriptionExpiredError()
 
-    def __handle_broken_cookies(self, response):
+        email_check_response = self.session.get(
+            AuthHandler.LEARNING_EMAIL_CHECK, params={'email': fields['email']}
+        ).json()
+
+        if not email_check_response['success']:
+            raise EmailError(email_check_response['message'])
+
+        password_name = register_get_response.find(
+            'input', {'type': 'password'}
+        )['name']
+
+        csrf_token = register_get_response.find(
+            'input', {'name': 'csrfmiddlewaretoken'}
+        )['value']
+
+        password_check_response = self.session.post(
+            AuthHandler.LEARNING_PASSWORD_CHECK,
+            data={
+                'csrfmiddlewaretoken': csrf_token,
+                password_name: fields['password'],
+                'field_name': password_name
+            }
+        ).json()
+
+        if not password_check_response['valid']:
+            raise PasswordError(password_check_response['msg'])
+
+        register_post_response = self.session.post(
+            AuthHandler.LEARNING_REGISTER,
+            data={
+                'next': '',
+                'trial_length': register_get_response.find(
+                    id='id_trial_length'
+                )['value'],
+                'csrfmiddlewaretoken': csrf_token,
+                'first_name': fields['first_name'],
+                'last_name': fields['last_name'],
+                'email': fields['email'],
+                'password1': fields['password'],
+                'country': fields['country'],
+                'referrer': fields['referrer'],
+                'recently_viewed_bits': '[]'
+            }
+        )
+
+        self.__handle_broken_cookies(register_post_response)
+
+        return self.session
+
+    def __handle_broken_cookies(self, response: Response):
         for cookie in response.raw.headers.getlist('Set-Cookie'):
             simple_cookie = SimpleCookie()
             simple_cookie.load(cookie)
--- src/handler/book_handler.py
+++ src/handler/book_handler.py
@@ -4,20 +4,18 @@ from urllib.parse import urljoin
 from ..constants.urls import Url
 from ..exceptions import InvalidSession
 
-class BookHandler():
+from .abstract_handler import AbstractHandler
+
+class BookHandler(AbstractHandler):
 
     LEARNING_BOOK = urljoin(Url.LEARNING, '/api/v1/book/{}/')
     LEARNING_BOOK_CHAPTER = urljoin(LEARNING_BOOK, 'chapter/')
-
-    def __init__(self, session):
-        self.session = session
 
     def get_book(self, book_id):
         pass
 
     def get_info(self, book_id):
-        if not self.session:
-            raise InvalidSession()
+        self._check_session()
 
         response = self.session.get(BookHandler.LEARNING_BOOK.format(book_id))
         if response.status_code == HTTPStatus.UNAUTHORIZED.value:
@@ -26,8 +24,7 @@ class BookHandler():
         return response.json()
 
     def get_chapters_info(self, book_id):
-        if not self.session:
-            raise InvalidSession()
+        self._check_session()
 
         return [
             chapter
