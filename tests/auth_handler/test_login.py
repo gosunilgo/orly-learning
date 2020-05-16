9e445583cda5c5155e21a50f218ef5b454102cd7
Alchemy-Meister <jesusesma@gmail.com>
2020-05-17 01:29:07 +0200

    implemented response fixture to mock server like dummy valid and invalid credential handling

--- tests/auth_handler/test_login.py
+++ tests/auth_handler/test_login.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+import json
 
 import pytest
 import responses
@@ -10,19 +11,27 @@ from orlylearning.handler import AuthHandler
 def init_auth():
     return AuthHandler(None, None)
 
-@responses.activate
-def test_authorized_login(auth):
-    responses.add(
-        responses.POST, AuthHandler.OREILLY_LOGIN, status=HTTPStatus.OK.value
-    )
-    auth.login('email', 'password')
+@pytest.fixture(name='_response')
+def init_response():
+    with responses.RequestsMock() as rsps:
+        rsps.add_callback(
+            responses.POST, AuthHandler.OREILLY_LOGIN, callback=login_callback
+        )
+        yield rsps
 
-@responses.activate
-def test_unauthorized_login(auth):
-    responses.add(
-        responses.POST,
-        AuthHandler.OREILLY_LOGIN,
-        status=HTTPStatus.BAD_REQUEST.value
-    )
+def login_callback(request):
+    request_body = json.loads(request.body)
+    if (
+            'valid' in request_body.get('email', '')
+            and 'valid' in request_body.get('password', '')
+    ):
+        return (HTTPStatus.OK.value, {}, '')
+
+    return (HTTPStatus.BAD_REQUEST, {}, '')
+
+def test_authorized_login(auth, _response):
+    auth.login('valid-email', 'valid-password')
+
+def test_unauthorized_login(auth, _response):
     with pytest.raises(InvalidCredentials):
-        auth.login('wrong.email', 'wrong-password')
+        auth.login('wrong-email', 'wrong-password')
